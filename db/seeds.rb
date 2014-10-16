920a55b650506e090f8ebeb0d9c1a184a6a41d3f
brimatteng <btenggren20@gmail.com>
2014-10-16 19:01:18 -0400

    Add associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.delete_all
+Category.delete_all
+Item.delete_all
+
+User.create!(email: Faker::Internet.email, password: 'password')
+User.create!(email: Faker::Internet.email, password: 'password')
+User.create!(email: Faker::Internet.email, password: 'password')
+User.create!(email: Faker::Internet.email, password: 'password')
+User.create!(email: Faker::Internet.email, password: 'password')
+
+Category.create!(name: 'Fire')
+Category.create!(name: 'Earth')
+Category.create!(name: 'Water')
+Category.create!(name: 'Air')
+
+Item.create!(name: Faker::Commerce.product_name, user_id: 1, category_id: 1)
+Item.create!(name: Faker::Commerce.product_name, user_id: 2, category_id: 2)
+Item.create!(name: Faker::Commerce.product_name, user_id: 3, category_id: 3)
+Item.create!(name: Faker::Commerce.product_name, user_id: 4, category_id: 4)
+Item.create!(name: Faker::Commerce.product_name, user_id: 5, category_id: 1)
+Item.create!(name: Faker::Commerce.product_name, user_id: 1, category_id: 2)
+Item.create!(name: Faker::Commerce.product_name, user_id: 2, category_id: 3)
+Item.create!(name: Faker::Commerce.product_name, user_id: 3, category_id: 4)
+Item.create!(name: Faker::Commerce.product_name, user_id: 4, category_id: 1)
+Item.create!(name: Faker::Commerce.product_name, user_id: 5, category_id: 2)
+
+
